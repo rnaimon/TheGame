fdce338cb3b8b4e4555855c3f64e1b97695a42dd
Michael <michael.katz.96@gmail.com>
2014-03-18 13:22:09 -0400

    This is the new Vertex Class that will be used in conjunction with a LineObject class to create the boundaries of the obstacles.

---
+++ src/Vertex.java
@@ -0,0 +1,52 @@
+public class Vertex
+ 
+{
+ 
+	private int xCoord;
+	private int yCoord;
+
+	
+ 
+	public Vertex(int x, int y)
+	{
+		xCoord=x;
+		yCoord=y;
+ 
+	}
+
+	public int getXCoord()
+ 
+	{
+ 
+		return xCoord;	
+
+	 }
+
+	public int getYCoord()
+ 
+	{
+ 
+		return yCoord;	
+
+	 }
+	
+	public void setXCoord(int x)
+
+	{
+		xCoord=x;
+ 
+	}
+ 
+	
+
+	public void setYCoord(int y)
+
+	 {
+ 
+		yCoord=y;
+ 
+	}
+ 
+	
+
+}
