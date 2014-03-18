625259ecafe58b32244dde118da0f72508d09844
Michael <michael.katz.96@gmail.com>
2014-03-18 13:52:28 -0400

    This is the interface for the generic level class.

---
+++ src/LevelInterface.java
@@ -0,0 +1,14 @@
+/*This interface is for the generic level class. 
+ * There will be an actual level class for each level.
+ */
+public interface LevelInterface
+{
+	public int getLevelNumber();
+	
+	public int setLevelNumber();
+	
+	public ArrayList<Obstacles> getObstacleList();
+	
+	public ArrayList<Item> getItemList();
+	
+}
