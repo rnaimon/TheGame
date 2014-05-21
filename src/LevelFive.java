41123b9b18cfb1fe77125a11a76b32d072c36ea0
michael.katz.96 <michael.katz.96@10.72.45.66>
2014-05-21 12:22:21 -0400

    Level Five environment.

--- src/LevelFive.java
+++ src/LevelFive.java
@@ -39,25 +39,21 @@ public class LevelFive extends Level {
 		super(p, width, height);
 		player=p;
 		player.setCentX(0);
-		player.setCentY(0);
-		setLevelNumber(5); //ALWAYS SET THIS CORRECTLY
+		player.setCentY(getGameHeight()-30);
+		setLevelNumber(5);
 		reset=false;
 		timer = 0;
 		amountWater = 0;
-		switchList= setUpSwitches();
+		switchList=null;
 		setObstacleList(setUpEnvironment());
 		player.setPlatforms(getObstacleList());
 		levelComplete=false;
-		
 		LP1 = new LifePreserver(super.getGameWidth()-200, super.getGameHeight() - amountWater*2, 3);
-
-		
 		breath = 4000;
-
-		for (int y = height; y > 0; y=y-2) {
+		for (int y = height; y > 0; y=y-2) 
+		{
 			
 			Rectangle rect = new Rectangle(0, y, width, 2);
-
 			
 			water.add(rect);
 		}
@@ -151,59 +147,19 @@ public class LevelFive extends Level {
 		Obstacles Platform7 = Platform6.translate(- gameWidth/6 - gameWidth/10, -(int)(gameHeight*((1.0/20.0))));
 		obstacleList.add(Platform7);
 		
-		Obstacles Platform8 = Platform7.translate(gameWidth/15, -(int)(gameHeight*((1.0/10.0))));
+		
+		Obstacles Platform7b = Platform7.translate(- gameWidth/6 - gameWidth/10, -(int)(gameHeight*((1.0/20.0))));
+		obstacleList.add(Platform7b);
+		
+		Obstacles Platform8 = Platform7b.translate(gameWidth/15, -(int)(gameHeight*((1.0/10.0))));
 		obstacleList.add(Platform8);
 		
 		Obstacles Platform9 = Platform8.translate(gameWidth/6 + gameWidth/10, -(int)(gameHeight*((1.0/20.0))));
 		obstacleList.add(Platform9);	
 		
-//		Obstacles Platform10 = Platform1.translate(0, 210);
-//		obstacleList.add(Platform10);
 		
-//		Obstacles Platform11 = Platform1.translate(350, 210);
-//		obstacleList.add(Platform11);
-		
-		//Obstacles Platform12 = Platform1.translate(700, 210);
-		//obstacleList.add(Platform12);
-		
-//		Obstacles Platform13 = Platform1.translate(0, 280);
-//		obstacleList.add(Platform13);
-		
-		//Obstacles Platform14 = Platform1.translate(350, 280);
-		//obstacleList.add(Platform14);
-		
-		//Obstacles Platform15 = Platform1.translate(700, 280);
-		//obstacleList.add(Platform15);
-		
-//		Obstacles Platform16 = Platform1.translate(0, 350);
-//		obstacleList.add(Platform16);
-		
-//		Obstacles Platform17 = Platform1.translate(0, 420);
-//		obstacleList.add(Platform17);		
-		
-		//Obstacles Platform18 = Platform1.translate(700, 420);
-		//obstacleList.add(Platform18);
-		
-		//Obstacles Platform19 = Platform1.translate(350, 350);
-		//obstacleList.add(Platform19);
-		
-		//Obstacles Platform20 = Platform1.translate(100, 400);
-		//obstacleList.add(Platform20);
-		
-		//Obstacles Platform21 = Platform1.translate(100, 350);
-		//obstacleList.add(Platform21);
-
-
-		
-		/* The following will eventually turn into a life preserver.*/
-		
-		ArrayList<LineObject> lifePreserver = new ArrayList<LineObject>();
-		LineObject LPTop= new LineObject(0, gameHeight-4*gameHeight/5, gameWidth/5, gameHeight-4*gameHeight/5);
-		LineObject LPSideL= new LineObject(0,gameHeight-4*gameHeight/5, 0, gameHeight-4*gameHeight/5 + 20);
-		LineObject LPBottom= new LineObject(0, gameHeight-4*gameHeight/5 + 20, gameWidth/5, gameHeight-4*gameHeight/5 + 20);
-		LineObject LPSideR= new LineObject(gameWidth/5, gameHeight-4*gameHeight/5, gameWidth/5, gameHeight-4*gameHeight/5 + 20);
-		
-		
+		Obstacles Platform11 = Platform9.translate(gameWidth/6 + gameWidth/10, -(int)(gameHeight*((1.0/20.0))));
+		obstacleList.add(Platform11);
 		
 		return obstacleList;
 
@@ -221,7 +177,7 @@ public class LevelFive extends Level {
 	
 	/***
 	 * This method contains code to allow the level-specific Item to have a certain 
-	 * function, if one were to exist. Nothing in this level, so far.
+	 * function, if one were to exist. Nothing in this level
 	 */
 	public void function() {
 		
@@ -253,47 +209,7 @@ public class LevelFive extends Level {
 		}
 	}
 	
-	 /***
-	 * Getter for the switchList arraylist
-	 * @return arraylist of switches in this level
-	 */
-	public ArrayList<Switch> getSwitchList()
-	{
-		return switchList;
-	}
 	
-	
-	public ArrayList<Switch> setUpSwitches() {
-		
-		
-		ArrayList<Switch> switches= new ArrayList<Switch>();
-
-		
-		ArrayList<LineObject> switch1 = new ArrayList<LineObject>();
-		
-		int x= 500;
-		int y=50;
-		
-		int gameHeight = super.getGameHeight();
-		int gameWidth = super.getGameWidth();
-		
-		// end goal switch is always first
-		LineObject Switch1Top= new LineObject(gameWidth-100, gameHeight - 15*(gameHeight/16), gameWidth-50, gameHeight - 15*(gameHeight/16));
-		LineObject Switch1SideL= new LineObject(gameWidth-100, gameHeight - 15*(gameHeight/16), gameWidth-100, gameHeight - 15*(gameHeight/16) + 50);
-		LineObject Switch1Bottom= new LineObject(gameWidth-100, gameHeight - 15*(gameHeight/16) + 50, gameWidth-50, gameHeight - 15*(gameHeight/16) + 50);
-		LineObject Switch1SideR= new LineObject(gameWidth-50, gameHeight - 15*(gameHeight/16), gameWidth-50, gameHeight - 15*(gameHeight/16) + 50);
-		
-		switch1.add(Switch1Top);
-		switch1.add(Switch1SideR);
-		switch1.add(Switch1Bottom);
-		switch1.add(Switch1SideL);
-
-		Switch s = new Switch(switch1);
-		
-		switches.add(s);
-		
-		return switches;
-	}
 
 	
 	public void draw(Graphics2D g) 
@@ -344,18 +260,15 @@ public class LevelFive extends Level {
 		
 		manageWater();
 	
-		for (int x = 0; x < amountWater; x++) {
+		for (int x = 0; x < amountWater; x++) 
+		{
 			g.fill(water.get(x).getBounds2D());
 		}
-		
-		
-		
-		
 		g.drawImage(LP1.getImage(), (int)LP1.getX(), (int)LP1.getY(), null);
 		
-		if(player.getCentX() - 4 > LP1.getX() && player.getCentX() + 4 < LP1.getX() + 100 && player.getCentY() + player.getRadius() < LP1.getY() + 30 && player.getCentY() + player.getRadius() > LP1.getY() +10) {
+		if(player.getCentX() - 4 > LP1.getX() && player.getCentX() + 4 < LP1.getX() + 100 && player.getCentY() + player.getRadius() < LP1.getY() + 30 && player.getCentY() + player.getRadius() > LP1.getY() +10) 
+		{
 			player.setGrounded(true);
-		//	player.setCentX(player.getCentX()-);
 		}
 		else
 			player.setGrounded(false);
@@ -385,36 +298,7 @@ public class LevelFive extends Level {
 			
 		}
 		else
-			breath = 4000;
-		
-
-		
-		
-		
-		
-		
-		
-		if(getSwitchList()!=null)
-		{
-			for(int i= 0; i< getSwitchList().size(); i++)
-			{
-				g.setColor(Color.green);
-				if(i!=0)
-				g.setColor(Color.white);
-				int[] vertx= new int[getSwitchList().get(i).getVertices().size()];
-				int[] verty= new int[getSwitchList().get(i).getVertices().size()];
-				for(int j=0; j< getSwitchList().get(i).getVertices().size(); j++)
-				{
-					vertx[j]=(int)(getSwitchList().get(i).getVertices().get(j).getXCoord());
-					verty[j]=(int)(getSwitchList().get(i).getVertices().get(j).getYCoord());
-				}
-				g.fillPolygon(vertx, verty, (getSwitchList().get(i).getVertices().size()));
-			}	
-		}
-		
-		
-			
-			
+			breath = 4000;	
 	}
 	
 	/***
